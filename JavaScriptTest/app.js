835b81b78b5e66e8cce2fbf5774d0e040d130a9f
souson7 <souson7@users.noreply.github.com>
2025-07-28 22:51:34 +0900

    演算子・繰り返し

--- JavaScriptTest/app.js
+++ JavaScriptTest/app.js
@@ -1,4 +1,9 @@
-const myname = "taro"
-console.log(myname)
-const myname = "jiro"
-console.log(myname)
+let score = 80;
+
+if(score >= 90){
+    alert('優秀');
+} else if(score >= 60){
+    alert('合格');
+} else{
+    alert('再試験');
+}
