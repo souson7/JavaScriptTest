d93415547cd4bc4fb097d389b5eef74ab2df49b4
souson7 <souson7@users.noreply.github.com>
2025-07-31 23:39:52 +0900

    関数の呼び出し

--- JavaScriptTest/app.js
+++ JavaScriptTest/app.js
@@ -1,9 +1,10 @@
-let score = 80;
+let myname = 'John';
 
-if(score >= 90){
-    alert('優秀');
-} else if(score >= 60){
-    alert('合格');
-} else{
-    alert('再試験');
+let hello = greet(myname);
+
+console.log(hello);
+
+function greet(name){
+    let greeting = 'Hello,' + name;
+    return greeting;
 }
