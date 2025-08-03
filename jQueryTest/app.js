4a1f13937e2decbf0ad5e3744c220529f2d0f274
souson7 <souson7@users.noreply.github.com>
2025-08-03 23:35:21 +0900

    jQuery

---
+++ jQueryTest/app.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.box').slideDown(function (){
+        $('.box').css({
+          'background-color': '#0000FF',
+          'width': '100px',
+          'height': '100px'
+        })
+    });
+});
